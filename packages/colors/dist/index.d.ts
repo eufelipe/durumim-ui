1ff33cc8cd39433715ec626ff75891f66dbd69f7
Felipe Rosas <contato@eufelipe.com>
2023-03-05 14:48:31 -0300

    chore(colors): remove dist folder

--- packages/colors/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare const colors: {
-    white: string;
-    black: string;
-    transparent: string;
-};
-
-export { colors };
--- packages/colors/dist/index.mjs
+++
@@ -1,9 +0,0 @@
-// src/colors.ts
-var colors = {
-  white: "#ffffff",
-  black: "#000000",
-  transparent: "transparent"
-};
-export {
-  colors
-};
