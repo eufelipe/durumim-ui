d8a1e366e56f68152d374bf51e5054a49f26cf89
Felipe Rosas <contato@eufelipe.com>
2023-04-13 22:11:43 -0300

    chore(docs): upgrade storybook version

--- packages/docs/.storybook/main.tsx
+++ packages/docs/.storybook/main.tsx
@@ -1,15 +1,31 @@
 module.exports = {
-  stories: ['../src/pages/**/*.stories.mdx', '../src/stories/**/*.stories.tsx'],
+  stories: ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.tsx"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
   ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-vite',
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
   },
   features: {
     storyStoreV7: true,
   },
-}
+  async viteFinal(config, { configType }) {
+    if (configType === "DEVELOPMENT") {
+      config.server.port = 6001;
+      config.server.https = false;
+      config.server.host = true;
+      config.server.hmr = {
+        port: 443,
+        protocol: "ws",
+      };
+    }
+    return config;
+  },
+  docs: {
+    autodocs: true,
+  },
+};
