7dcd0ca3eb5cfb055fa417114c46f53a38910654
Felipe Rosas <contato@eufelipe.com>
2023-04-14 23:47:21 -0300

    chore(docs): change preview

--- packages/docs/.storybook/preview.tsx
+++ packages/docs/.storybook/preview.tsx
@@ -1,16 +1,26 @@
-import durumimTheme from './durumimTheme'
-import './global.css'
+import durumimTheme from "./durumimTheme";
+import "./global.css";
 
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  layout: 'centered',
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  layout: "fullscreen",
+  options: {
+    theme: {
+      mainBorder: "1px solid blue",
+    },
+    storySort: (a, b) =>
+      a.id === b.id
+        ? 0
+        : a.id.localeCompare(b.id, undefined, { numeric: true }),
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
+
   docs: {
     theme: durumimTheme,
   },
-}
+};
