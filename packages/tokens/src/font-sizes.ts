a44d32adc3531a60c2ec9a8c534c9ff9a4018778
Felipe Rosas <contato@eufelipe.com>
2023-04-14 22:56:45 -0300

    chore: add font size tokens

---
+++ packages/tokens/src/font-sizes.ts
@@ -0,0 +1,16 @@
+export const fontSizes = {
+  xxs: "0.625rem", // 10px
+  xs: "0.75rem", // 12px
+  sm: "0.875rem", // 14px
+  md: "1rem", // 16px
+  lg: "1.125rem", // 18px
+  xl: "1.25rem", // 20px
+  "2xl": "1.5rem", // 24px
+  "3xl": "1.875rem", // 30px
+  "4xl": "2rem", // 32px
+  "5xl": "3rem", // 48px
+  "6xl": "4rem", // 64px
+  "7xl": "5rem", // 80px
+  "8xl": "6rem", // 96px
+  "9xl": "7rem", // 112px
+};
