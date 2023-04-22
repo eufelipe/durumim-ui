8484c9a83c672c8e2748cbbed8414b17e45a4e89
Felipe Rosas <contato@eufelipe.com>
2023-04-22 16:24:37 -0300

    chore(tokens): add shadow tokens

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -3,4 +3,5 @@ export * from "./font-weight";
 export * from "./fonts";
 export * from "./line-height";
 export * from "./radii";
+export * from "./shadows";
 export * from "./space";
---
+++ packages/tokens/src/shadows.ts
@@ -0,0 +1,7 @@
+export const shadows = {
+  none: "none",
+  sm: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;",
+  md: "rgba(0, 0, 0, 0.35) 0px 5px 15px;",
+  lg: "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;",
+  xl: "rgba(0, 0, 0, 0.56) 0px 22px 70px 4px;",
+};
