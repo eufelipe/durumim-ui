e69d308eebc9676d73efd90cfacd16e74bfd444d
Felipe Rosas <contato@eufelipe.com>
2025-03-22 15:06:17 -0300

    chore: update colors design token

--- packages/design-tokens/src/colors.ts
+++ packages/design-tokens/src/colors.ts
@@ -1,88 +1,131 @@
-export const accent = "#82a26aff";
-export const secondary = "#4ba695ff";
-export const primary = "#1f6d8cff";
-export const alert = "#993629ff";
-export const zinc = "#27272aff";
-export const stone = "#1c1917ff";
-export const dark = "#290000ff";
-export const slate = "#0f172aff";
-export const light = "#d5d5d5ff";
-export const success = "#2a655fff";
-export const info = "#2d9ec8ff";
-export const warning = "#f97316ff";
-export const danger = "#993629ff";
-export const white = "#ffffffff";
-export const black = "#000000ff";
-export const purify = "#dde4eaff";
-export const lightness = "#ebebf0ff";
-export const clean = "#f3f2f7ff";
-export const gray50 = "#e7eaeeff";
-export const gray100 = "#b8bfccff";
-export const gray200 = "#8994a9ff";
-export const gray300 = "#717e98ff";
-export const gray400 = "#5e6a82ff";
-export const gray500 = "#4d566aff";
-export const gray600 = "#3c4353ff";
-export const gray700 = "#222222ff";
-export const gray800 = "#1a1d23ff";
-export const gray900 = "#090a0cff";
-export const green50 = "#f1f9f8ff";
-export const green100 = "#c5e7e3ff";
-export const green200 = "#b7e1dcff";
-export const green300 = "#9ad5ceff";
-export const green400 = "#6fc3b9ff";
-export const green500 = "#4ba695ff";
-export const green600 = "#368179ff";
-export const green700 = "#2a655fff";
-export const green800 = "#1e4844ff";
-export const green900 = "#122b29ff";
-export const red50 = "#f9cec7ff";
-export const red100 = "#f49d90ff";
-export const red200 = "#f07c6aff";
-export const red300 = "#ed5c45ff";
-export const red400 = "#e93b20ff";
-export const red500 = "#c12913ff";
-export const red600 = "#a62511ff";
-export const red700 = "#811d0eff";
-export const red800 = "#5c150aff";
-export const red900 = "#370d06ff";
-export const blue50 = "#bce2f0ff";
-export const blue100 = "#79c4e2ff";
-export const blue200 = "#58b5daff";
-export const blue300 = "#36a7d3ff";
-export const blue400 = "#288fb8ff";
-export const blue500 = "#1f6d8cff";
-export const blue600 = "#1a5b75ff";
-export const blue700 = "#124154ff";
-export const blue800 = "#0b2732ff";
-export const blue900 = "#071a21ff";
-export const yellow50 = "#fdfdd8ff";
-export const yellow100 = "#fbf99dff";
-export const yellow200 = "#f9f776ff";
-export const yellow300 = "#f8f54fff";
-export const yellow400 = "#f6f328ff";
-export const yellow500 = "#e2e00aff";
-export const yellow600 = "#c4c108ff";
-export const yellow700 = "#9d9a07ff";
-export const yellow800 = "#757306ff";
-export const yellow900 = "#4e4c04ff";
-export const orange50 = "#ffdec2ff";
-export const orange100 = "#ffc999ff";
-export const orange200 = "#ffb370ff";
-export const orange300 = "#ff9d47ff";
-export const orange400 = "#ff871fff";
-export const orange500 = "#f57200ff";
-export const orange600 = "#cc5f00ff";
-export const orange700 = "#a34c00ff";
-export const orange800 = "#7a3900ff";
-export const orange900 = "#522600ff";
-export const pink50 = "#ffc2e5ff";
-export const pink100 = "#ff85ccff";
-export const pink200 = "#ff5cbbff";
-export const pink300 = "#ff33aaff";
-export const pink400 = "#ff0a99ff";
-export const pink500 = "#da0080ff";
-export const pink600 = "#b8006bff";
-export const pink700 = "#8f0053ff";
-export const pink800 = "#66003cff";
-export const pink900 = "#3d0024ff";
+export const colors = {
+  // brand colors
+  accent: "#82a26aff",
+  secondary: "#4ba695ff",
+  primary: "#1f6d8cff",
+  alert: "#993629ff",
+
+  // neutral colors
+  zinc: "#27272aff",
+  stone: "#1c1917ff",
+  dark: "#290000ff",
+  slate: "#0f172aff",
+  light: "#d5d5d5ff",
+  white: "#ffffffff",
+  black: "#000000ff",
+
+  // state colors
+  success: "#2A655F",
+  info: "#2D9EC8",
+  warning: "#F97316",
+  danger: "#993629",
+
+  // light state colors
+  successLight: "#4BA695",
+  infoLight: "#8BCDE5",
+  warningLight: "#F9954D",
+  dangerLight: "#FF3333",
+
+  // background colors
+  purify: "#dde4eaff",
+  lightness: "#ebebf0ff",
+  clean: "#f3f2f7ff",
+
+  // gray scale
+  gray: {
+    50: "#e7eaeeff",
+    100: "#b8bfccff",
+    200: "#8994a9ff",
+    300: "#717e98ff",
+    400: "#5e6a82ff",
+    500: "#4d566aff",
+    600: "#3c4353ff",
+    700: "#222222ff",
+    800: "#1a1d23ff",
+    900: "#090a0cff",
+  },
+
+  // green scale
+  green: {
+    50: "#f1f9f8ff",
+    100: "#c5e7e3ff",
+    200: "#b7e1dcff",
+    300: "#9ad5ceff",
+    400: "#6fc3b9ff",
+    500: "#4ba695ff",
+    600: "#368179ff",
+    700: "#2a655fff",
+    800: "#1e4844ff",
+    900: "#122b29ff",
+  },
+
+  // red scale
+  red: {
+    50: "#f9cec7ff",
+    100: "#f49d90ff",
+    200: "#f07c6aff",
+    300: "#ed5c45ff",
+    400: "#e93b20ff",
+    500: "#c12913ff",
+    600: "#a62511ff",
+    700: "#811d0eff",
+    800: "#5c150aff",
+    900: "#370d06ff",
+  },
+
+  // blue scale
+  blue: {
+    50: "#bce2f0ff",
+    100: "#79c4e2ff",
+    200: "#58b5daff",
+    300: "#36a7d3ff",
+    400: "#288fb8ff",
+    500: "#1f6d8cff",
+    600: "#1a5b75ff",
+    700: "#124154ff",
+    800: "#0b2732ff",
+    900: "#071a21ff",
+  },
+
+  // yellow scale
+  yellow: {
+    50: "#fdfdd8ff",
+    100: "#fbf99dff",
+    200: "#f9f776ff",
+    300: "#f8f54fff",
+    400: "#f6f328ff",
+    500: "#e2e00aff",
+    600: "#c4c108ff",
+    700: "#9d9a07ff",
+    800: "#757306ff",
+    900: "#4e4c04ff",
+  },
+
+  // orange scale
+  orange: {
+    50: "#ffdec2ff",
+    100: "#ffc999ff",
+    200: "#ffb370ff",
+    300: "#ff9d47ff",
+    400: "#ff871fff",
+    500: "#f57200ff",
+    600: "#cc5f00ff",
+    700: "#a34c00ff",
+    800: "#7a3900ff",
+    900: "#522600ff",
+  },
+
+  // pink scale
+  pink: {
+    50: "#ffc2e5ff",
+    100: "#ff85ccff",
+    200: "#ff5cbbff",
+    300: "#ff33aaff",
+    400: "#ff0a99ff",
+    500: "#da0080ff",
+    600: "#b8006bff",
+    700: "#8f0053ff",
+    800: "#66003cff",
+    900: "#3d0024ff",
+  },
+} as const;
